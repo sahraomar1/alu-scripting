aff6d7cab2d7f21b5b521830e608c5d624a4c895
Sahraomar1 <s.omar@alustudent.com>
2025-07-12 19:51:47 +0300

    What

--- api_advanced/1-top_ten.py
+++ api_advanced/1-top_ten.py
@@ -1,23 +1,35 @@
 #!/usr/bin/python3
-"""get hot post function"""
-
-
-import json
+"""
+This module contains the function top_ten.
+"""
 import requests
-import sys
+from sys import argv
 
 
 def top_ten(subreddit):
-    """get top ten hot post"""
-    if len(sys.argv) < 2:
-        return print(None)
+    """
+    Returns the top ten posts for a given subreddit.
+    """
+    user = {'User-Agent': 'Mozilla/5.0'}
+    url = 'https://www.reddit.com/r/{}/hot/.json?limit=10'.format(subreddit)
+
+    try:
+        response = requests.get(url, headers=user, allow_redirects=False)
+        response.raise_for_status()
+        data = response.json()
+
+        if 'data' in data and 'children' in data['data']:
+            for post in data['data']['children']:
+                print(post['data']['title'])
+        print("OK")
+    except requests.exceptions.RequestException:
+        print("None")
+    except ValueError:
+        print("None")
+
+
+if __name__ == "__main__":
+    if len(argv) > 1:
+        top_ten(argv[1])
     else:
-        url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
-        headers = {"User-Agent": "Mozilla/5.0"}
-        result = requests.get(url, headers=headers, allow_redirects=False)
-        listing = []
-        if result.status_code != 200:
-            return print(None)
-        body = json.loads(result.text)
-        for i in body["data"]["children"]:
-            print(i["data"]["title"])
+        print("Usage: ./script.py <subreddit>")
